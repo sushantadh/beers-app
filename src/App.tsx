423cff7e588811e9a4eb600cbe9f6cfeccca2e35
Sushant Adhikari <sushant@tekkon.com.np>
2023-07-08 22:19:25 +0545

    linter and prettier installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import './App.css';
+import './App.css'
 
 function App() {
-	return <div className='App'>APP</div>;
+  return <div className='App'>APP</div>
 }
 
-export default App;
+export default App
