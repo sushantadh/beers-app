40307be37dca9b2f52e3cdda0830e6249e9dd3f5
Sushant Adhikari <sushant@tekkon.com.np>
2023-07-10 13:49:10 +0545

    Handled responsive placing for add new beer botton

--- src/components/beerList/myBeers/AddBeerToolbar.tsx
+++ src/components/beerList/myBeers/AddBeerToolbar.tsx
@@ -3,7 +3,7 @@ import { Button, Container } from 'react-bootstrap'
 function AddBeerToolbar(props: AddBeerProps) {
   const { onAddBeer } = props
   return (
-    <Container className='mt-3 pl-0 d-flex justify-content-end'>
+    <Container className='mt-3 ps-0 d-flex justify-content-center justify-content-md-end '>
       <Button type='button' variant='primary' onClick={onAddBeer}>
         Add a new beer
       </Button>
